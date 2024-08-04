bd8ccf5bf382bbca62e52ce487e984c6365b015f
Fawzi B <fawzi@breidi.com>
2024-08-04 18:34:28 -0400

    fixed couple of bugs

--- plex_backup.sh
+++ plex_backup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# https://github.com/fawzib/complete_plex_backup
 
 # Define variables
 BACKUP_FOLDER="/mnt/user/appdata/binhex-plexpass/Plex Media Server"
-#BACKUP_FOLDER=/mnt/user/Downloads/youtube
+#BACKUP_FOLDER="/tmp/test_data"
 REMOTE_SERVER="google"
-REMOTE_DESTINATION="plex_app_data_zip"
+#REMOTE_DESTINATION="test_backup"
+REMOTE_DESTINATION="plex_app_data"
 TMP_FOLDER="/tmp/plex_backup"
 BACKUP_DATE=$(date +%Y-%m-%d)
 BACKUP_DESTINATION="$REMOTE_DESTINATION/$BACKUP_DATE"
@@ -35,39 +37,32 @@ remove_old_backups() {
 # Function to acquire lock and run a command
 run_with_lock() {
   local command="$1"
-  local retries=0
 
-  #echo "Starting run_with_lock with command: $command, background: $background"
+ (
+    # Attempt to acquire a lock on the folder
+    exec 200>"$LOCK_FILE"
+    retry_count=0
 
-  while [ $retries -lt $FLOCK_MAX_RETRIES ]; do
-    (
-      # Try to acquire the lock
-      flock -n 200
-      if [ $? -eq 0 ]; then
-        #echo "Lock acquired, executing command: $command"
-        
-        # Lock acquired, execute the command
+    while ! flock -n 200; do
+        if [ "$retry_count" -ge "$FLOCK_MAX_RETRIES" ]; then
+            echo "Max retries reached for $folder. Skipping..."
+            break
+        fi
+        echo "Could not acquire lock. Retrying in $FLOCK_RETRY_INTERVAL seconds... ($((retry_count+1))/$FLOCK_MAX_RETRIES)"
+        sleep "$FLOCK_RETRY_INTERVAL"
+        retry_count=$((retry_count + 1))
+    done
+
+    if [ "$retry_count" -lt "$FLOCK_MAX_RETRIES" ]; then
         eval "$command"
         command_status=$?
-
-        if [ $command_status -ne 0 ]; then
+		
+		if [ $command_status -ne 0 ]; then
           echo "Command failed with status $command_status."
           exit $command_status
         fi
-
-        echo "Command executed successfully and lock released."
-        exit 0
-      else
-        # Failed to acquire the lock, increment the retry counter and sleep
-        retries=$((retries + 1))
-        echo "Failed to acquire lock. Attempt $retries/$FLOCK_MAX_RETRIES. Retrying in $FLOCK_RETRY_INTERVAL seconds..."
-        sleep $FLOCK_RETRY_INTERVAL
-      fi
-    ) 200>"$LOCK_FILE"
-  done
-
-  echo "Failed to acquire lock after $FLOCK_MAX_RETRIES attempts."
-  exit 1
+    fi
+  ) 200>"$LOCK_FILE"
 }
 
 # Function to stop containers gracefully
@@ -102,6 +97,28 @@ file_uploaded() {
     return 1
 }
 
+
+clean_filename() {
+    local original_name="$1"
+    local cleaned_name
+
+    # Replace spaces with underscores
+    cleaned_name=$(echo "$original_name" | tr ' ' '_')
+
+    # Replace non-Latin characters with Latin equivalents using iconv
+    cleaned_name=$(echo "$cleaned_name" | iconv -f UTF-8 -t ASCII//TRANSLIT)
+
+    # Remove any characters that are not alphanumeric, underscores, or dashes
+    cleaned_name=$(echo "$cleaned_name" | tr -cd '[:alnum:]_-')
+
+    # Ensure the cleaned name is not empty
+    if [[ -z "$cleaned_name" && -n "$original_name" ]]; then
+        cleaned_name="default_filename"
+    fi
+
+    echo "$cleaned_name"
+}
+
 zip_and_upload() {
     local folder_path=$1
     local zip_base_name=$2
@@ -113,18 +130,32 @@ zip_and_upload() {
         echo "Skipping empty folder: $folder_to_backup"
         return
     fi
+	
+	cleaned_filename=$(clean_filename "$zip_base_name")
 
     # Start the zip process in the background
-	command="zip -r -$COMPRESSION_LEVEL -q -s $SPLIT_SIZE \"$TMP_FOLDER/$zip_base_name.zip\" \"$folder_to_backup\"/* 2>\"$ERROR_LOG\""
+
+	full_file_path="$TMP_FOLDER/$cleaned_filename.zip"
+
+	# Check if the file exists
+	if [ -f "$full_file_path" ]; then
+		echo "File $full_file_path already exists."
+		rm "$full_file_path"
+		echo "File $full_file_path has been deleted."
+	fi
+	
+	command="zip -r -$COMPRESSION_LEVEL -q -s $SPLIT_SIZE \"$full_file_path\" \"$folder_to_backup\"/* 2>\"$ERROR_LOG\""
+	echo $command
 	run_with_lock "$command" &
-    #(zip -r -0 -q -s $SPLIT_SIZE "$TMP_FOLDER/$zip_base_name.zip" "$folder_to_backup"/* 2> "$ERROR_LOG") &
+    #(zip -r -0 -q -s $SPLIT_SIZE "$full_file_path" "$folder_to_backup"/* 2> "$ERROR_LOG") &
 	zip_pid=$!
 
-    # Loop to monitor the zip process and upload new files
+	echo $zip_pid
+    # Loop to monitor the zip process and upload new 
+
     while kill -0 "$zip_pid" 2>/dev/null; do
-			
-        # Find new files that have not been uploaded yet
-        for file in $(ls "$TMP_FOLDER" | grep -E "${zip_base_name}.zip|${zip_base_name}.z[0-9]*"); do
+        for file in $(ls "$TMP_FOLDER" | grep -E "${cleaned_filename}.zip|${cleaned_filename}.z[0-9]*"); do
+		    #echo "Found file: $file"
             if ! file_uploaded "$file"; then
                 echo "Uploading file: $file"
                 rclone move "$TMP_FOLDER/$file" "$REMOTE_SERVER:$BACKUP_DESTINATION/" \
@@ -146,9 +177,10 @@ zip_and_upload() {
         sleep 10
     done
 
+	echo "Uploading leftovers..."
     # Final upload in case any parts were missed during parallel upload
     rclone move "$TMP_FOLDER/" "$REMOTE_SERVER:$BACKUP_DESTINATION/" \
-        --include "${zip_base_name}.zip" --include "${zip_base_name}.z[0-9]*" \
+        --include "${cleaned_filename}.zip" --include "${cleaned_filename}.z[0-9]*" \
 		--size-only \
         --check-first \
         --retries 20 \
@@ -163,11 +195,22 @@ zip_and_upload() {
         --quiet
 }
 
+BACKUP_FOLDER="${BACKUP_FOLDER%/}"
+TMP_FOLDER="${TMP_FOLDER%/}"
 
-if [ -f "$LOCK_FILE" ]; then
-  echo "Lock file exists. Exiting script."
-  exit 1
+# Ensure we are using bash
+if [ -z "$BASH_VERSION" ]; then
+    echo "This script requires bash to run"
+    exit 1
 fi
+
+shopt -s extglob
+
+
+#if [ -f "$LOCK_FILE" ]; then
+#  echo "Lock file exists. Exiting script."
+#  exit 1
+#fi
 
 mkdir -p "$TMP_FOLDER"
 uploaded_files=()  # Initialize the array
@@ -219,7 +262,7 @@ for ITEM in "$BACKUP_FOLDER"/*; do
 done
 
 rm $LOCK_FILE
-# Remove old backups, keeping only the last 3
+# Remove old backups, keeping only the last RETENTION_COUNT
 remove_old_backups "$REMOTE_DESTINATION" "$RETENTION_COUNT"
 echo "Backup and upload completed successfully, and old backups have been pruned."
 
